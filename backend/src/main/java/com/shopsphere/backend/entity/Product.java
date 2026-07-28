054805f1299b9e83be1c59aaf98cbec46596b411
Kumar Aryan <sushantkraryan@gmail.com>
2026-07-28 19:18:42 +0530

    update: update Product file

--- backend/src/main/java/com/shopsphere/backend/entity/Product.java
+++ backend/src/main/java/com/shopsphere/backend/entity/Product.java
@@ -1,7 +1,18 @@
 package com.shopsphere.backend.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
