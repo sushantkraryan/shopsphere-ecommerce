ab9fc26feee65410c62b49e12d49d5329d77cb8f
Kumar Aryan <sushantkraryan@gmail.com>
2026-07-30 17:05:28 +0530

    feat: add ProductService with business logic for product creation and retrieval

---
+++ backend/src/main/java/com/shopsphere/backend/service/CategoryService.java
@@ -0,0 +1,8 @@
+package com.shopsphere.backend.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+
+}
---
+++ backend/src/main/java/com/shopsphere/backend/service/ProductService.java
@@ -0,0 +1,62 @@
+package com.shopsphere.backend.service;
+
+import com.shopsphere.backend.dto.ProductDTO;
+import com.shopsphere.backend.entity.Category;
+import com.shopsphere.backend.entity.Product;
+import com.shopsphere.backend.mapper.ProductMapper;
+import com.shopsphere.backend.repository.CategoryRepository;
+import com.shopsphere.backend.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+    private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
+    private final ProductMapper productMapper;
+
+    public ProductService(ProductRepository productRepository, CategoryRepository categoryRepository, ProductMapper productMapper) {
+        this.productRepository = productRepository;
+        this.categoryRepository = categoryRepository;
+        this.productMapper = productMapper;
+    }
+
+    @Transactional(readOnly = true)
+    public List<ProductDTO> getAllProducts() {
+        return productRepository.findAll()
+                .stream()
+                .map(productMapper::toDTO)
+                .toList();
+    }
+
+    @Transactional(readOnly = true)
+    public ProductDTO getProductById(Long id) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Product not found with id: " + id));
+        return productMapper.toDTO(product);
+    }
+
+    @Transactional
+    public ProductDTO createProduct(ProductDTO dto) {
+        Category category = categoryRepository.findById(dto.getCategoryId())
+                .orElseThrow(() -> new RuntimeException(("Category not found with idd: " + dto.getCategoryId())));
+
+        if (productRepository.existsBySku(dto.getSku())) {
+            throw new RuntimeException("SKU already exists: " + dto.getSku());
+        }
+
+        Product product = Product.builder()
+                .name(dto.getName())
+                .description(dto.getDescription())
+                .price(dto.getPrice())
+                .stockQuantity(dto.getStockQuantity())
+                .sku(dto.getSku())
+                .category(category)
+                .build();
+
+        Product saved = productRepository.save(product);
+        return productMapper.toDTO(saved);
+    }
+}
